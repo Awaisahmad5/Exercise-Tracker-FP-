3d8f7bf0da35abcb9f2496b77e644d2ef4833d7c
Awais Ahmad Zaidi <49332806+Awaisahmad5@users.noreply.github.com>
2023-02-16 17:17:12 +0500

    All done

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ var app = express();
 var model = require("./model");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
-const cors = require("cors"); //s11 yaha se hm fe be attach kren ge
+const cors = require("cors"); 
 
 const port = 4000;
 
@@ -18,13 +18,7 @@ app.post("/post", (req, res) => {
   });
 });
 
-//s2 (read) get wla
-// app.get("/read", async (req, res) => {
-//    // let kettleBell = await model.find({}); //fin k andr flowr {} se sara data wha show hojata h
-//    const exrcise = "Running"
-//    let kettleBell = await model.find({});
-//     res.send(kettleBell);
-// });
+
 
 app.get("/read/:activity", async (req, res) => {
   const exr = req.params.activity;
@@ -34,22 +28,22 @@ app.get("/read/:activity", async (req, res) => {
 });
 
 app.delete("/del/:id", async (req, res) => {
-  await model.findByIdAndDelete({ _id: req.params.id }); // s3 uper line me colon k agy jo name diay hy jesy idr "id" hy to wo or params. k bad wla name jo k idr "id" hi hy same hony chye. jo _id underscore wli ye db me jo id hoti hy wo hy
+  await model.findByIdAndDelete({ _id: req.params.id }); 
   res.json("deleted");
 });
 
-// s2 ye edit wla step hy mtlb aik 1 id se data find kr rhy hian
+
 app.get("/edit/:id", async (req, res) => {
-  let hoja = await model.findById(req.params.id); //ye id variable hy. hm ne return krwany k lia hoja me save krwa lia
+  let hoja = await model.findById(req.params.id); 
   res.json(hoja);
 });
 
-// s3 update k liay hum post use kren ge
+
 app.post("/update/:id", async (req, res) => {
   let upHoja = await model.findByIdAndUpdate(
     { _id: req.params.id },
     { $set: req.body }
-  ); //$set wla ye replace krdy ga pichly wla {} wala data ko
+  ); 
   res.json(upHoja);
 });
 
